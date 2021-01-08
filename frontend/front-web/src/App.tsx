0c83e0d5ca9d113fba3d05c1e841f9247fc0cf4b
Jeandes <jeandeswesley@hotmail.com>
2021-01-08 06:39:02 -0300

    Add home page

--- frontend/front-web/src/App.tsx
+++ frontend/front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
 
-    <h1>Hello</h1>
+         <Navbar />
+         <Home />
 
     </div>
   );
