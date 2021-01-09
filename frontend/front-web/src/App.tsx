dd1d780549ab1cdc36103cb61f8c0cbc5fa93665
Jeandes <jeandeswesley@hotmail.com>
2021-01-09 10:07:00 -0300

    Finish front-web

--- frontend/front-web/src/App.tsx
+++ frontend/front-web/src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
-import Routes from './Routes'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
+import Routes from "./Routes";
 
 function App() {
   return (
-            <Routes />
-    );
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
